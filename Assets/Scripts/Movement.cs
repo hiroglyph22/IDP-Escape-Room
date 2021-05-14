d3d8e61325575f04daeddf4eded00298491b1b69
hiroglyph22 <66212654+hiroglyph22@users.noreply.github.com>
2021-05-14 15:40:21 -0700

    added tile mapper and more assets

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -17,7 +17,7 @@ public class Movement : MonoBehaviour
 
     void Update()
     {
-        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), -1*Input.GetAxis("Vertical"));
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         moveVelocity = moveInput.normalized * speed;
     }
 
