74eb49336ef670cb726dff18b6cc3fb15979c7d5
hiroglyph22 <66212654+hiroglyph22@users.noreply.github.com>
2021-05-18 16:27:41 -0700

    Finished Intro Screen

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class DialogueManager : MonoBehaviour
 {
@@ -11,9 +12,24 @@ public class DialogueManager : MonoBehaviour
 
     public bool dialogActive;
 
+    public string[] dialogLines;
+    
+    public int currentLine;
+
+    public DialogHolder dH;
+
+    public GameObject inputField;
+
+    public InputField theInputField;
+
+    public string username;
+
+    public GameObject nextScene;
+
+
     void Start()
     {
-        
+        dH = FindObjectOfType<DialogHolder>();
     }
 
     // Update is called once per frame
@@ -21,9 +37,41 @@ public class DialogueManager : MonoBehaviour
     {
         if (dialogActive && Input.GetKeyDown(KeyCode.Space))
         {
+                //dBox.SetActive(false);
+                //dialogActive = false;
+            currentLine++;
+            if (dH.dialogueLines[currentLine] == "What is your username?")
+            {
+                inputField.SetActive(true);
+            }
+            try
+            {
+                if (dH.dialogueLines[currentLine - 1] == "What is your username?")
+                {
+                    username = theInputField.text;
+                    inputField.SetActive(false);
+                    print(username);
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+            if (currentLine == 9)
+            {
+                nextScene.SetActive(true);
+            }
+        }
+
+        if (currentLine >= dialogLines.Length)
+        {
             dBox.SetActive(false);
             dialogActive = false;
+
+            currentLine = 0;
         }
+
+        dText.text = dialogLines[currentLine];
     }
 
     public void ShowBox(string dialogue)
@@ -31,5 +79,11 @@ public class DialogueManager : MonoBehaviour
         dialogActive = true;
         dBox.SetActive(true);
         dText.text = dialogue;
+    }
+
+    public void ShowDialogue()
+    {
+        dialogActive = true;
+        dBox.SetActive(true);
     }
 }
